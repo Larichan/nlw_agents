8347ce7063ba2dbb36e32824bfbf29bfbb51b8e9
larichan01 <larissa_noronha01@hotmail.com>
2025-07-12 23:30:57 -0300

    feat: adição de IA para transcrição e geração de respostas

--- server/src/http/routes/create-question.ts
+++ server/src/http/routes/create-question.ts
@@ -2,6 +2,8 @@ import { FastifyPluginCallbackZod } from "fastify-type-provider-zod"
 import { z } from "zod/v4"
 import { db } from "../../db/connection.ts"
 import { schema } from "../../db/schema/index.ts"
+import { generateAnswer, generateEmbeddings } from "../../services/gemini.ts"
+import { and, eq, sql } from "drizzle-orm"
 
 export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
     app.post("/rooms/:roomId/questions", {
@@ -18,9 +20,36 @@ export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
             const { question } = body
             const { roomId } = params
 
+            const embeddings = await generateEmbeddings(question)
+
+            const embeddingsAsString = `[${embeddings.join(",")}]`
+
+            const chunks = await db.select({
+                id: schema.audioChunks.id,
+                transcription: schema.audioChunks.transcription,
+                similarity: sql<number>`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector)`
+            }).from(schema.audioChunks)
+                .where(
+                    and(
+                        eq(schema.audioChunks.roomId, roomId),
+                        sql`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector) > 0.7`
+                    )
+                )
+                .orderBy(sql`${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector`)
+                .limit(3)
+
+            let answer: string | null = null
+
+            if (chunks.length > 0) {
+                const transcriptions = chunks.map(chunk => chunk.transcription)
+
+                answer = await generateAnswer(question, transcriptions)
+            }
+
             const result = await db.insert(schema.questions).values({
                 roomId,
-                question
+                question,
+                answer
             }).returning()
 
             const insertedQuestion = result[0]
@@ -29,6 +58,6 @@ export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
                 return reply.status(500).send({ error: "Failed to create a question" })
             }
 
-            return reply.status(201).send({ questionId: insertedQuestion.id })
+            return reply.status(201).send({ questionId: insertedQuestion.id, answer })
         })
 }
--- server/src/http/routes/upload-audio.ts
+++ server/src/http/routes/upload-audio.ts
@@ -1,5 +1,8 @@
 import { FastifyPluginCallbackZod } from "fastify-type-provider-zod"
 import { z } from "zod/v4"
+import { generateEmbeddings, transcribeAudio } from "../../services/gemini.ts"
+import { db } from "../../db/connection.ts"
+import { schema } from "../../db/schema/index.ts"
 
 export const uploadAudioRoute: FastifyPluginCallbackZod = (app) => {
     app.post("/rooms/:roomId/audio", {
@@ -17,6 +20,24 @@ export const uploadAudioRoute: FastifyPluginCallbackZod = (app) => {
                 throw new Error("No audio file provided")
             }
 
-            return reply.status(201)
+            const audioAsBase64 = await audio.toBuffer().then(buffer => buffer.toString("base64"))
+
+            const transcription = await transcribeAudio(audioAsBase64, audio.mimetype)
+
+            const embeddings = await generateEmbeddings(transcription)
+
+            const result = await db.insert(schema.audioChunks).values({
+                roomId,
+                transcription,
+                embeddings
+            }).returning()
+
+            const chunk = result[0]
+
+            if (!chunk) {
+                throw new Error("Erro ao salvar chunk de áudio")
+            }
+
+            return reply.status(201).send({ chunkId: chunk.id })
         })
 }
